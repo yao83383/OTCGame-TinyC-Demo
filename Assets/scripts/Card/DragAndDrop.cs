1a9b1d6ab67ca8afa53e74261360b4861b347211
yao83383 <326613318@qq.com>
2024-03-31 13:53:35 +0800

    -fix 相机和Card移动

--- Assets/scripts/Card/DragAndDrop.cs
+++ Assets/scripts/Card/DragAndDrop.cs
@@ -7,43 +7,97 @@ using UnityEngine;
 public class DragAndDrop : MonoBehaviour
 {
     private Camera mainCamera; // 引用主相机
+    private CameraControllerSettings CamCtrlSettings;
     private Vector3 screenPoint; // 鼠标在屏幕上的初始位置
     private Vector3 offset; // 物体在世界空间中的初始偏移量
-    private bool isDragging = false; // 是否正在拖拽
+    private bool isCardDragging = false; // 是否正在拖拽
+    private bool isCameraDragging = false;
+    private RaycastHit hit;
+    private Vector3 m_prevPosition;
 
+    private GameObject ObjectToMove;
     void Start()
     {
         mainCamera = Camera.main; // 获取主相机
+        CamCtrlSettings = this.GetComponent<CameraControllerSettings>();
     }
 
     void Update()
     {
-        if (isDragging)
-        {
-            // 计算鼠标在屏幕上的位置
-            Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+#if (UNITY_ANDROID || UNITY_IPHONE) && !UNITY_EDITOR
+			HandleTouchInput();
+#else
+        HandleMouseInput();
+#endif
+        
+    }
 
-            // 将屏幕坐标转换为世界坐标
-            Vector3 cursorPosition = mainCamera.ScreenToWorldPoint(cursorPoint) + offset;
-            cursorPosition.z = -1;
-            // 更新物体的位置
-            transform.position = cursorPosition;
+    void HandleTouchInput()
+    {
+    }
+    void HandleMouseInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                ObjectToMove = hit.collider.gameObject;
+                m_prevPosition = Input.mousePosition;
+                if (hit.collider.gameObject.CompareTag("Map"))
+                {
+                    isCameraDragging = true;
+                    Debug.Log("Click Map");
+                }
+                else if (hit.collider.gameObject.CompareTag("Card"))
+                {
+                    screenPoint = mainCamera.WorldToScreenPoint(ObjectToMove.transform.position);
+                    offset = ObjectToMove.transform.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+                    isCardDragging = true;
+                    Debug.Log("Click Card");
+                }
+            }
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            if (isCardDragging)
+            {
+                // 计算鼠标在屏幕上的位置
+                Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+                //Debug.Log(Input.mousePosition);
+                // 将屏幕坐标转换为世界坐标
+                Vector3 cursorPosition = mainCamera.ScreenToWorldPoint(cursorPoint) + offset;
+                cursorPosition.z = -1;
+                // 更新物体的位置
+                ObjectToMove.transform.position = cursorPosition;
+                Debug.Log(cursorPosition);
+            }
+            else if (isCameraDragging)
+            {
+                Vector2 curMousePosition = Input.mousePosition;
+                MoveCamera(m_prevPosition, curMousePosition);
+                m_prevPosition = curMousePosition;
+            }
+        }
+        else
+        {
+            isCardDragging = false;
+            isCameraDragging = false;
+            ObjectToMove = null;
         }
     }
 
-    void OnMouseDown()
+    private void MoveCamera(Vector2 prevPosition, Vector2 curPosition)
     {
-        // 当鼠标按下时，记录屏幕坐标和物体的偏移量
-        screenPoint = mainCamera.WorldToScreenPoint(transform.position);
-        offset = transform.position - mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-
-        // 开始拖拽
-        isDragging = true;
-    }
-
-    void OnMouseUp()
-    {
-        // 当鼠标释放时，停止拖拽
-        isDragging = false;
+        //注意这里的myCamera.nearClipPlaen。由于我使用的是透视相机，所以需要将z值改为这个
+        //如果读者使用的是正交相机，可能不需要这个
+        Vector2 offset = (Camera.main.ScreenToWorldPoint(new Vector3(prevPosition.x, prevPosition.y, Camera.main.nearClipPlane))
+            - Camera.main.ScreenToWorldPoint(new Vector3(curPosition.x, curPosition.y, Camera.main.nearClipPlane)));
+        //这里的m_cameraScale,因为我不想修改nearClipPlaen的值来达到移动的快慢，所以加了个移动参数
+        Vector2 newPos = new Vector2(transform.localPosition.x + offset.x * CamCtrlSettings.m_cameraScaleVal, transform.localPosition.y + offset.y * CamCtrlSettings.m_cameraScaleVal);
+        newPos.y = Mathf.Clamp(newPos.y, CamCtrlSettings.m_minCamYPos, CamCtrlSettings.m_maxCamYPos);
+        newPos.x = Mathf.Clamp(newPos.x, CamCtrlSettings.m_minCamXPos, CamCtrlSettings.m_maxCamXPos);
+        transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
     }
 }
+
