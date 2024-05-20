62b667f6b43b6c94536f9da5bf63b4506afa1782
姚更 <yg@aihuashen.com>
2024-05-20 22:28:09 +0800

    -fix 添加Savelode 删除冗余代码

--- Assets/scripts/TC2/TC2WonderName.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-[Serializable]
-public class TC2WonderName
-{
-	public string name;
-
-	public string english;
-}
--- Assets/scripts/WonderName.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-[Serializable]
-public class WonderName
-{
-	public string name;
-
-	public string english;
-}
