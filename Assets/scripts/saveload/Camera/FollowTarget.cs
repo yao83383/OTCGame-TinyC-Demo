ac61da0fd26e89aac3c7b7a2397efedab124a586
姚更 <yg@aihuashen.com>
2024-05-20 23:10:42 +0800

    -fix 替换回input system old

--- Assets/scripts/saveload/Camera/FollowTarget.cs
+++ Assets/scripts/saveload/Camera/FollowTarget.cs
@@ -5,40 +5,40 @@ using UnityEngine;
 // so that the camera follows the player (or whatever the target is).
 public class FollowTarget : MonoBehaviour
 {
-    [Header("Transform to Follow")]
-    [SerializeField] private Transform targetTransform;
+    //[Header("Transform to Follow")]
+    //[SerializeField] private Transform targetTransform;
 
-    [Header("Configuration")]
-    [SerializeField] private bool followX = true;
-    [SerializeField] private bool followY = true;
-    [SerializeField] private Vector2 offset = Vector2.zero;
+    //[Header("Configuration")]
+    //[SerializeField] private bool followX = true;
+    //[SerializeField] private bool followY = true;
+    //[SerializeField] private Vector2 offset = Vector2.zero;
 
-    private Transform originalTargetTransform;
+    //private Transform originalTargetTransform;
 
-    private void Start() 
-    {
-        originalTargetTransform = targetTransform;
-    }
+    //private void Start() 
+    //{
+    //    originalTargetTransform = targetTransform;
+    //}
 
-    private void LateUpdate() 
-    {
-        // if we don't have a target transform, don't update
-        if (targetTransform == null) 
-        {
-            return;
-        }
+    //private void LateUpdate() 
+    //{
+    //    // if we don't have a target transform, don't update
+    //    if (targetTransform == null) 
+    //    {
+    //        return;
+    //    }
 
-        float newPosX = this.transform.position.x;
-        float newPosY = this.transform.position.y;
-        if (followX) 
-        {
-            newPosX = targetTransform.position.x + offset.x;
-        }
-        if (followY) 
-        {
-            newPosY = targetTransform.position.y + offset.y;
-        }
-        this.transform.position = new Vector3(newPosX, newPosY, this.transform.position.z);
-    }
+    //    float newPosX = this.transform.position.x;
+    //    float newPosY = this.transform.position.y;
+    //    if (followX) 
+    //    {
+    //        newPosX = targetTransform.position.x + offset.x;
+    //    }
+    //    if (followY) 
+    //    {
+    //        newPosY = targetTransform.position.y + offset.y;
+    //    }
+    //    this.transform.position = new Vector3(newPosX, newPosY, this.transform.position.z);
+    //}
 
 }
